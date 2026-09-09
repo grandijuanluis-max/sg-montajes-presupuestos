b8c71f0a62b26d22d3fafe6790fcdae6fa4a765c
grandijuanluis-max <grandijuanluis@gmail.com>
2026-09-09 12:57:05 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4923,7 +4923,7 @@ window.abrirRobotF6 = function() {
         const userVendedorCodigo = currentUser ? currentUser.vendedor_codigo : '';
         let filteredClients = clientesDB;
         if (userVendedorCodigo) {
-            filteredClients = clientesDB.filter(c => c.vendedor_id === userVendedorCodigo);
+            filteredClients = clientesDB.filter(c => c.vendedor_id === userVendedorCodigo || !c.vendedor_id || c.vendedor_id === '');
         }
         
         if (cleanQuery === '') {
